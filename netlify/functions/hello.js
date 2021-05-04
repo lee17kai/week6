b8ce9fb4f66e2ba53424ea34772260e67cf749d6
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-05-04 09:04:41 -0500

    0904

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,14 @@
 // defines a lambda function
 exports.handler = async function(event) {
 
+  // YOU can write stuff here!
+
+
   // a lambda function returns a status code and a string of data
   return {
-    statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status this url tells you all the meanings. usually 200
     body: `Hello from the back-end!` // a string of data
   }
+  
+
 }
